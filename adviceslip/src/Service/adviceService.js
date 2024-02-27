a22b46b96721e406be2425d9e16fa6813e28631c
revuss <sarathteja14@gmail.com>
2024-02-27 22:52:59 +0530

    Upd

--- adviceslip/src/Service/adviceService.js
+++ adviceslip/src/Service/adviceService.js
@@ -1,20 +1,15 @@
-
-
-
 const fetchAdvice = async () => {
-    try {
-        const response = await fetch("https://api.adviceslip.com/advice");
-        if (!response.ok) {
-            throw new Error('Network response was not ok');
-        }
-        const data = await response.json();
-        return data.slip.advice;
-    } catch (error) {
-        console.error('Error fetching advice:', error);
-        throw error;
+  try {
+    const response = await fetch("https://api.adviceslip.com/advice");
+    if (!response.ok) {
+      throw new Error("Network response was not ok");
     }
-    
+    const data = await response.json();
+    return data.slip.advice;
+  } catch (error) {
+    console.error("Error fetching advice:", error);
+    throw error;
+  }
 };
-
 
 export { fetchAdvice };
